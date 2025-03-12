46f7f35226f3049b8db307f6dc3d30e0d63fee80
Andy Deng <andy.z.deng@gmail.com>
2025-03-12 06:06:41 +0000

    fix imported symbol rename bug

---
+++ src/try/fileB.ts
@@ -0,0 +1,120 @@
+import { funcA as funcABC, varA } from './fileA'
+export { varA } from './fileA'
+
+export const varB = 'world'
+
+export const { age: myAge } = varA
+export const [myBook1, myBook2] = varA.books
+
+const yourAge = myAge
+
+const funcB = (word: string) => funcABC() + ' ' + varB + word
+
+export {
+    funcB as funcB1,
+    myFunc1,
+    ISome,
+}
+
+type IHello = {
+    readonly name: string
+    readonly addr: string
+}
+
+type IWorld = {
+    age: number
+    hello?: IHello
+}
+
+const temp: IWorld[] = []
+
+interface ISome extends IWorld {
+    func1(p1: number, p2: string): void
+}
+
+class ClassA implements ISome {
+    name: string = 'hi'
+    age: number = 10
+    hello?: IHello
+
+    func1(p1: number, p2: string): void {
+        this.func2(p1, p2)
+        const age = p1
+        const addr = p2
+        temp.push({ age, hello: { name: p2, addr } })
+    }
+
+    funcOfClassA() {
+        //
+    }
+
+    private func2(p1: number, p2: string): void {
+        console.log('name is', this.name)
+        const { name } = this
+    }
+}
+
+/**
+ * @param p3 some thing
+ * @returns wrong
+ */
+function myFunc1(p3: number, p4: boolean): number {
+    const ccc = funcB
+    funcB('hi')
+    return 0
+}
+
+export const myFunc2 = (p1: number, p2: boolean): number => {
+    return myFunc1(p1, p2)
+}
+
+export function myFunc3(p3: number, p4: boolean): number {
+    return myFunc2(p3, p4)
+}
+
+type MyType = {
+    prop1: string
+    method1: () => string
+    method2(param1: string, param2: number): void
+}
+
+export interface IMyInterface extends MyType {
+    some: ISome
+}
+
+export type Abc = ISome & {
+    some1: number
+}
+
+type Abcd = Abc & {
+    world: string
+}
+
+class What implements Abcd {
+    some1: number = 1
+    hello?: IHello
+    name: string = 'hi'
+    func1(p1: number, p2: string): void {
+        throw new Error('Method not implemented.')
+    }
+    world: string = 'world'
+    age: number = 11
+}
+
+class Where extends What {
+    func2(p1: number, p2: string): void {
+        throw new Error('Method not implemented.')
+    }
+}
+
+export class Why extends What {
+    newField?: Where
+
+    func1(p1Some: number, p2Some: string): void {
+        throw new Error('Method not implemented.')
+    }
+
+    func2(p1: string): ClassA {
+        throw new Error('Method not implemented.')
+    }
+}
---
+++ src/try/tryMinify.ts
@@ -0,0 +1,16 @@
+import ts from 'typescript'
+import { minify, MinifierOptions, writeDestFile } from '../minify'
+
+const configFile = ts.readJsonConfigFile('tsconfig.json', ts.sys.readFile)
+
+const minifierOptions: MinifierOptions = {
+    srcDir: '/opt/workspace/minify-ts/src/try',
+    destDir: '/opt/workspace/minify-ts/min/src/try',
+    interfaceFileArr: [
+        'fileB.ts',
+    ],
+    obfuscate: true,
+}
+
+const compilerOptions = ts.parseJsonSourceFileConfigFileContent(configFile, ts.sys, '/opt/workspace/minify-ts/')
+minify(minifierOptions, writeDestFile, compilerOptions.options)
